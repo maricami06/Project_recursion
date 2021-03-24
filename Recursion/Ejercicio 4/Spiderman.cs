e824c2f403ef937a3726084190160e99482dca11
maricami06 <mcherrera63538@umanizales.edu.co>
2021-03-23 22:55:20 -0500

    act

---
+++ Recursion/Ejercicio 4/Spiderman.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Recursion.Ejercicio_4
+{
+    class Spiderman
+    {
+        public static void Main(string[] args)
+        {
+            int num = 3;
+            int total = PossibleSteps(num,1);
+            Console.WriteLine("total is: " + total);
+        }
+        public static int PossibleSteps(int num, int i)
+        {
+            if (num >= 2)
+            {
+                return PossibleSteps(num - 1, i) + PossibleSteps(num - 2,i);
+            }
+            else if (num == i)
+            {
+                    return (num);
+               
+            }
+            else
+            {
+                return i;
+            }
+        }
+    }
+}
